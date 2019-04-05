5e0e05bb7537b99a7d596317b60935a0ca434cb0
jyp114110 <1140967775@qq.com>
2019-04-05 17:26:16 +0800

    06-仪表盘

---
+++ js/map.js
@@ -0,0 +1,5 @@
+(function () {
+    window.HOST_TYPE = "2";
+    window.BMap_loadScriptTime = (new Date).getTime();
+    document.write('<script type="text/javascript" src="public/echarts/bmap.js"></script>');
+})();
